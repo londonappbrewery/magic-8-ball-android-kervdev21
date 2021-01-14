8b35f54e705421572cc0a9f6212f3e7abab283c9
kervin philippe <55342854+kervdev21@users.noreply.github.com>
2021-01-14 03:44:37 -0500

    Challenge Completed!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,40 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity
+{
+    private ImageView ballDisplay;
+    private int[] ballArray;
+    private Button myButton;
+    Random randomNumGenerator;
+    private int number;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ballDisplay = (ImageView) findViewById(R.id.image_eightBall);
+        ballArray = new int[] {R.drawable.ball1, R.drawable.ball2, R.drawable.ball3, R.drawable.ball4, R.drawable.ball5};
+        myButton = (Button) findViewById(R.id.askButton);
+
+
+        myButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                randomNumGenerator = new Random();
+                number = randomNumGenerator.nextInt(5);
+                ballDisplay.setImageResource(ballArray[number]);
+            }
+        });
     }
 }
